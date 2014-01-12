8d03e6b94d59f5169c278835136321f66e13b059
David Balderston <david@howtoinstallghost.com>
2014-01-11 17:55:09 -0800

    initial commit

---
+++ updateghost_digitalocean.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
+# updateghost_digitalocean.sh will update your current Digital Ocean Ghost install to the latest version without you losing any content
+
+if [[ `whoami` != root ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+#Stop Ghost
+service ghost stop
+
+#Make Tempory Directory and Download Lates Ghost
+cd /var/www/ghost
+mkdir temp
+cd temp
+wget https://github.com/TryGhost/Ghost/releases/download/0.4.0-pre2/Ghost-0.4.0-pre2.zip
+unzip *.zip
+cd ..
+
+#Make Backup DB
+cp content/data/ghost.db content/data/ghost_backup.db
+echo "###### Data Backed Up ######"
+
+#Copy the new files over
+yes | cp temp/*.md temp/*.js temp/*.json .
+rm -R core
+yes | cp -R temp/core .
+yes | cp -R temp/content/themes/casper content/themes
+npm install --production
+echo "###### NPM Installed ######"
+
+#Delete temp folder
+rm -R temp
+
+#Start Ghost Again
+service ghost start
+echo "###### Ghost Started ######"
