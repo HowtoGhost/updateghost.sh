a3ff7959809e1cd4236f74ab25df9450618695d5
David Balderston <david@howtoinstallghost.com>
2014-03-21 22:48:10 -0700

    Adding Amazon pm2 Update Script

---
+++ updateghost_ami_pm2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
+# updateghost_ami.sh will update your current Amazon ami ghost install to the latest version without you losing any content
+
+if [[ `whoami` != root ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+#Stop Ghost
+pm2 stop all
+
+#Make Tempory Directory and Download Lates Ghost
+cd /var/www/ghost
+mkdir temp
+cd temp
+wget https://ghost.org/zip/ghost-latest.zip
+unzip *.zip
+cd ..
+
+#Make Backup DB
+cp content/data/ghost.db content/data/ghost_backup.db
+echo "###### Data Backed Up ######"
+
+#Copy the new files over
+yes | cp temp/*.md temp/*.js temp/*.json .
+rm -R core
+yes | cp -R temp/core .
+yes | cp -R temp/content/themes/casper content/themes
+npm install --production
+echo "###### NPM Installed ######"
+
+#Delete temp folder
+rm -R temp
+
+#Start Ghost Again
+pm2 start index.js --name ghost
+echo "###### Ghost Started ######"
